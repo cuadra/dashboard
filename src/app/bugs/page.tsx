c67ede172cb47677a41adce7d1d2bef44ba508eb
Jose <jose.cuadra@live.com>
2026-03-08 23:43:31 -0400

    typescript fixes

--- src/app/bugs/page.tsx
+++ src/app/bugs/page.tsx
@@ -1,7 +1,10 @@
 import json from "@/src/data/2026-03-09/errors.json";
 import * as stylex from "@stylexjs/stylex";
 import typography from "@/src/styles/typography";
+
 export default function Bugs() {
+  const errorMessages = json.errorMessages as Record<string, string[]>;
+
   return (
     <>
       <header>
@@ -9,16 +12,18 @@ export default function Bugs() {
       </header>
 
       <ul>
-        {Object.keys(json.errorMessages).map((url, errors) => (
-          <details key={url} open>
-            <summary>{url}</summary>
+        {Object.entries(errorMessages).map(([url, errors]) => (
+          <li key={url}>
+            <details open>
+              <summary>{url}</summary>
 
-            <ul>
-              {json.errorMessages[url].map((error, index) => (
-                <li key={index}>{error}</li>
-              ))}
-            </ul>
-          </details>
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={`${url}-${index}`}>{error}</li>
+                ))}
+              </ul>
+            </details>
+          </li>
         ))}
       </ul>
     </>
